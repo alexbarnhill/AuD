6866a8bf95c3804e8e22678a95b3b97a5d7eaa39
Alexander Barnhill <arbl@methodpark.de>
2017-10-21 19:17:25 +0200

    Added method bodies for every method except for the delta methods

--- ersteUebung/Physics.java
+++ ersteUebung/Physics.java
@@ -1,26 +1,41 @@
 public class Physics {
+
+	private static double AVOGARDO = 6.0221409e+23;
+
+	// m2 kg s-2 K-1
+	private static double BOLTZMANN = 1.38064852e-23;
+	//
+	// p · V = N · kB · T
+
 	// Given gas-volume v (m*m*m) and temperature t (K),
 	// this method computes and returns the pressure p (Pa).
+	// p = (N * kB * T) / V
 	public static double computeP(double v, double t) {
-		return 0.0; // TODO
+		double pressure = (AVOGARDO * BOLTZMANN * t) / v;
+		return pressure;
 	}
 
 	// Given pressure p (Pa) and temperature t (K),
 	// this method computes and returns the gas-volume v (m*m*m).
+	// V = (N * kB * T / V) / p
 	public static double computeV(double p, double t) {
-		return 0.0; // TODO
+		double volume = (AVOGARDO * BOLTZMANN * t) / p;
+		return volume;
 	}
 
 	// Given pressure p (Pa) and gas-volume v (m*m*m),
 	// this method computes and returns the temperature t (K).
+	// T = (p * V) / (N * kB);
 	public static double computeT(double p, double v) {
-		return 0.0; // TODO
+		double temperature = (p * v) / (AVOGARDO * BOLTZMANN);
+		return temperature;
 	}
 
 	// Given gas-volume v (m*m*m) and a change in temperature of deltaT (K),
 	// this method computes and returns the change in pressure (Pa).
+	//
 	public static double computeDeltaPisochore(double v, double deltaT) {
-		return 0.0; // TODO
+		return 0.0;
 	}
 
 	// Given gas-volume v (m*m*m), temperature t (K) and a change in volume of deltaV (m*m*m),
@@ -31,11 +46,15 @@ public class Physics {
 
 	// Given temperature t (K) and molar mass of particle m (kg/mol),
 	// this method computes and returns the average speed of a particle.
+	// 1/2(m)v^2 = 3/2 (kB) T
+	// v = sqrt(3kB * T / m)
 	public static double computeAverageSpeed(double t, double m) {
-		return 0.0; // TODO
+		double velocity = Math.sqrt((3 * BOLTZMANN * t) / m);
+		return velocity;
 	}
 
 	public static void main(String[] args) {
-		
+		double v = computeAverageSpeed(373, 1.9944235e-26);
+		System.out.println(v);
 	}
 }
