71ed5bdfb625f1568d676d5b51ec77ea197eda8f
Alexander Barnhill <arbl@methodpark.de>
2017-10-21 17:33:43 +0200

    First commit.Basic Physics.java class with main method

---
+++ ersteUebung/Physics.java
@@ -0,0 +1,41 @@
+public class Physics {
+	// Given gas-volume v (m*m*m) and temperature t (K),
+	// this method computes and returns the pressure p (Pa).
+	public static double computeP(double v, double t) {
+		return 0.0; // TODO
+	}
+
+	// Given pressure p (Pa) and temperature t (K),
+	// this method computes and returns the gas-volume v (m*m*m).
+	public static double computeV(double p, double t) {
+		return 0.0; // TODO
+	}
+
+	// Given pressure p (Pa) and gas-volume v (m*m*m),
+	// this method computes and returns the temperature t (K).
+	public static double computeT(double p, double v) {
+		return 0.0; // TODO
+	}
+
+	// Given gas-volume v (m*m*m) and a change in temperature of deltaT (K),
+	// this method computes and returns the change in pressure (Pa).
+	public static double computeDeltaPisochore(double v, double deltaT) {
+		return 0.0; // TODO
+	}
+
+	// Given gas-volume v (m*m*m), temperature t (K) and a change in volume of deltaV (m*m*m),
+	// this method computes and returns the change in pressure (Pa).
+	public static double computeDeltaPisotherm(double v, double t, double deltaV) {
+		return 0.0; // TODO
+	}
+
+	// Given temperature t (K) and molar mass of particle m (kg/mol),
+	// this method computes and returns the average speed of a particle.
+	public static double computeAverageSpeed(double t, double m) {
+		return 0.0; // TODO
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
